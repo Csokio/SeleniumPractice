549ceb6dbdc009afef7b86fb730c677b576e04ab
Sőregi Krisztián <krisztiansoreg@gmail.com>
2023-05-13 14:20:28 +0200

    commit

--- src/main/java/Pages.java
+++ src/main/java/Pages.java
@@ -19,26 +19,26 @@ public abstract class Pages {
 
     protected static WebDriver driver;
 
-    protected Pages()
-    {
-        if(driver == null){
-        WebDriverManager.chromedriver().setup();
+    public Pages() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            //System.setProperty("webdriver.chrome.driver", "chromedriver");
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("ignore-certificate-errors");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-extensions");
-        //options.addArguments("--headless");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("start-maximized");
-        options.addArguments("--remote-allow-origins=*");
-
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));}
+            ChromeOptions options = new ChromeOptions();
+            options.setAcceptInsecureCerts(true);
+            options.addArguments("ignore-certificate-errors");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-notifications");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--headless");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("start-maximized");
+            options.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        }
     }
-
     public static void closeDriver()
     {
         if(driver != null){
